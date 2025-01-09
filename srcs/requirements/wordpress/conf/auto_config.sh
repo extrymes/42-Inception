257d99b97e5193d55d1711a0ff5d4e463621e291
extrymes <65893656+extrymes@users.noreply.github.com>
2025-01-09 23:33:18 +0100

    feat(wordpress): add auto_config.sh and www config file

---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,13 @@
+sleep 10
+
+wp config create --allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+if ! [ -d "/run/php" ] then
+	mkdir -p /run/php
+fi
+
+/usr/sbin/php-fpm7.3 -F
