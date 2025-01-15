6f0f21c209349abcdfdf5c2cf02208efb465a41e
extrymes <65893656+extrymes@users.noreply.github.com>
2025-01-15 14:07:19 +0100

    fix(mariadb): grant all privileges to root

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,6 +8,7 @@ mysql -u root -p$DB_ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mysql -u root -p$DB_ROOT_PASS -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';"
 mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
 mysql -u root -p$DB_ROOT_PASS -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
+mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';"
 mysql -u root -p$DB_ROOT_PASS -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$DB_ROOT_PASS shutdown
